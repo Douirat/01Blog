460b7d4339b1541f3ecfee2987b7a2ae7d7ab1c2
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-11-09 15:36:13 +0100

    component 50%

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+// import { AdminComponent } from './features/admin/admin.component'; TODO: when you want to add the admine component it should be guarded
+// import { roleGuard } from './core/auth/role.guard'; // import the admin panel guarder.
+// import { httpInterceptor } from './core/authentication/interceptors/http-interceptor';
+import { Registeration } from './features/authentication/registeration/registeration';
+export const routes: Routes = [
+//   { path: 'admin', component: AdminComponent, canActivate: [roleGuard] }, // the admin panel with its gate keeper.
+// other routes...
+// { path: 'login', component: LoginComponent },
+  { path: 'register', component: Registeration },
+//   { 
+//     path: 'dashboard', 
+//     component: DashboardComponent,
+//     canActivate: [httpInterceptor]  // Protected route
+//   },
+//   { 
+//     path: 'profile', 
+//     component: ProfileComponent,
+//     canActivate: [httpInterceptor]  // Protected route
+//   }
+];
