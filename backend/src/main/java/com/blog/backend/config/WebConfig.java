8cf52a63a42cef56a7fad1ca99c7cbb6ab98b0a0
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-11-27 02:24:51 +0100

    the image is displayed v2

--- backend/src/main/java/com/blog/backend/config/WebConfig.java
+++ backend/src/main/java/com/blog/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.blog.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        // Ensure trailing slash exists
+        if (!uploadDir.endsWith("/")) {
+            uploadDir = uploadDir + "/";
+        }
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/home/bdouirat/Desktop/01Blog/backend/uploads/");
+                .addResourceLocations("file:" + uploadDir);
     }
 }
