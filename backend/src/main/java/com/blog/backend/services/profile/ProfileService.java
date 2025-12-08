6d77be4d3e592bc64c9f6e37efb90967c79276d4
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-12-08 15:12:11 +0100

    creating the service

--- backend/src/main/java/com/blog/backend/services/profile/ProfileService.java
+++ backend/src/main/java/com/blog/backend/services/profile/ProfileService.java
@@ -1,6 +1,8 @@
 package com.blog.backend.services.profile;
 
-public interface ProfileService {
+import com.blog.backend.dto.PaginatedUsersDTO;
+import org.springframework.data.domain.Page;
 
-     
+public interface ProfileService {
+Page<PaginatedUsersDTO> fetchUsers(int page);
 }
--- backend/src/main/java/com/blog/backend/services/profile/ProfileServiceImpl.java
+++ backend/src/main/java/com/blog/backend/services/profile/ProfileServiceImpl.java
@@ -1,8 +1,22 @@
 package com.blog.backend.services.profile;
 
 import com.blog.backend.services.profile.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.blog.backend.dtos.user.PaginatedUsersDTO;
+// import com.blog.backend.dtos.
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 
 public class ProfileServiceImpl implements ProfileService {
+    @Autowired
+    private UserRepository userRepository;
 
-     
+    @Override
+    public Page<PaginatedUsersDTO> fetchUsers(int page) {
+        int size = 20;
+        Pageable pageable = PageRequest.of(page, size);
+        Page<> posts = userRepository.findAll(pageable);
+    }
+
 }
